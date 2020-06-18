35d9a7cbd04d0eb09e625906ff65b67566580c9c
Jlynngreen <jessgreen2012@gmail.com>
2020-06-18 14:33:30 -0600

    Drill 124

---
+++ Drill 124/Drill 124/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a Number?");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = Number == 88;
+
+            do
+            {
+                switch (Number)
+                {
+                    case 42:
+                        Console.WriteLine("You guessed 42. Try again.");
+                        Console.WriteLine("Guess a Number?");
+                        Number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 33:
+                        Console.WriteLine("You guessed 33. Try again.");
+                        Console.WriteLine("Guess a Number?");
+                        Number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 88:
+                        Console.WriteLine("you guessed the Number 88. That is correct!");
+                        isGuessed = true;
+                        break;
+                }
+            }
+            while (!isGuessed);
+            
+             Console.ReadLine();
+        }
+    }
+}
