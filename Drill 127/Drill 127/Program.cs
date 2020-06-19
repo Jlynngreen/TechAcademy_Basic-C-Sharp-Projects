6db99a1fc86b491f11040aa71b6e7f0e10cea8f6
Jlynngreen <jessgreen2012@gmail.com>
2020-06-19 16:36:52 -0600

    Drill 127

---
+++ Drill 127/Drill 127/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_127
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "jess";
+            string quote = "They said, \"Hi\", jess. \n Hi its finally the weekend. \n \t yay can't wait.";
+
+
+            string author = "Robin Williams";
+            Console.WriteLine(author);
+
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(" My name is Jess,");
+
+            sb.AppendLine(" I Like C#!");
+
+            sb.AppendLine("Can't wait to move out of utah one day.");
+
+            Console.WriteLine(sb);
+
+
+
+            Console.WriteLine(quote);
+            Console.ReadLine();
+        }
+    }
+}
