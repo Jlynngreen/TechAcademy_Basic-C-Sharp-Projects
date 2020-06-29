a905f080f02a73cfebd6611a91391e60d452b5f1
Jlynngreen <jessgreen2012@gmail.com>
2020-06-29 12:14:51 -0600

    Drill 152

---
+++ Drill 152/Drill 152/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_152
+{
+    class Class1
+    {
+        public int Sum(int x)
+        {
+
+            int result = x + 35;
+
+            return result;
+        }
+
+
+
+
+        public int Substraction(int x)
+        {
+
+            int result = x - 10;
+
+            return result;
+        }
+
+
+        public int division(int x)
+        {
+
+            int result = x / 15;
+
+            return result;
+        }
+    }
+}
---
+++ Drill 152/Drill 152/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Class1 mathclass = new Class1();
+            Console.WriteLine(mathclass.Sum(num));
+
+            Class1 mathclass1 = new Class1();
+            Console.WriteLine(mathclass1.Substraction(num));
+
+            Class1 mathclass2 = new Class1();
+            Console.WriteLine(mathclass2.division(num));
+
+            Console.ReadLine();
+        }
+    }
+
+}
