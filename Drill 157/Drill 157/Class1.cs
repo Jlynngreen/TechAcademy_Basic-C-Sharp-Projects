f5bec4e9d8061e07edd65f9fe9ddf487f15ebca0
Jlynngreen <jessgreen2012@gmail.com>
2020-06-30 12:47:59 -0600

    Drill 157

---
+++ Drill 157/Drill 157/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_157
+{
+    class Class1
+    {
+        public void MyMethod(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        
+    
+        
+    }
+}
+
---
+++ Drill 157/Drill 157/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 add = new Class1();
+            add.MyMethod(x:44, y:34);
+            Console.ReadLine();
+            
+        }
+    }
+}
