024f94ca238401d03890b44c340bae1cf0507f33
Jlynngreen <jessgreen2012@gmail.com>
2020-06-30 14:21:57 -0600

    Drill 160

---
+++ Drill 160/Drill 160/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_160
+{
+     static class Class1
+    {
+        public static void MyMethod(int x)
+        {
+            int div = x / 2;
+            Console.WriteLine("results:" + div);
+
+            
+        }
+        public static void MyMethod(int x, out int div)
+        {
+            div = x * 2;
+            
+
+        }
+    }
+}
---
+++ Drill 160/Drill 160/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_160
+{
+    class Program
+    {
+      
+            static void Main(string[] args)
+            {
+                //Class1 div = new Class1();
+
+                Console.WriteLine("Please enter a number");
+                int user = Convert.ToInt32(Console.ReadLine());
+                Class1.MyMethod(user);
+                Console.WriteLine("Please enter another number");
+                int user2 = Convert.ToInt32(Console.ReadLine());
+                int num;
+                Class1.MyMethod(user2, out num);
+                Console.WriteLine("your results:" + num);
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+ 
