8043a06d532b4f60aa85bacb7cc98d6feb2e4eed
Jlynngreen <jessgreen2012@gmail.com>
2020-06-30 11:33:48 -0600

    Drill 155

---
+++ Drill 155/Drill 155/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_155
+{
+    class Class1
+    {
+        
+        public int Sum(int x)
+        {
+
+            int result = x + 35;
+
+            return result;
+        }
+        public int Sum(decimal x)
+        {
+
+            int result = (int)(x - 98.5m);
+
+            return result;
+        }
+
+
+        public int Sum(string x)
+        {
+            int myNum = Int32.Parse(x);
+            int result = myNum + 15;
+            return result;
+        }
+    }
+}
---
+++ Drill 155/Drill 155/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_155
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Class1 mathclass = new Class1();
+            Console.WriteLine(mathclass.Sum(num));
+
+            Console.WriteLine("User input a decimal");
+            decimal dec = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(mathclass.Sum(dec));
+
+            Console.WriteLine("user put in a number");
+            string word = Console.ReadLine();
+            Console.WriteLine(mathclass.Sum(word));
+
+            
+            Console.ReadLine();
+        }
+    }
+}
