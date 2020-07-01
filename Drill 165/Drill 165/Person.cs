0b08b7a19a05da32654d1d299da2edd37c77f076
Jlynngreen <jessgreen2012@gmail.com>
2020-07-01 12:41:19 -0600

    Drill 165

---
+++ Drill 165/Drill 165/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_165
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + " " + lastName);
+        }
+    }
+
+}
+
+
