8afbe744c4ff2b92ebec54b857716db7b6ac95d2
Jlynngreen <jessgreen2012@gmail.com>
2020-07-03 14:18:55 -0600

    Drill 181

---
+++ Drill 181/Drill 181/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_181
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string> { "coffee", "paper", "pencil" };
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int> { 10, 20, 30, 40 };
+
+            foreach (string thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+                
+            }
+            foreach (int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
