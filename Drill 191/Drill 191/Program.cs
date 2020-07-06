7552a092868fc71ef208626e96c8f1023a303dfc
Jlynngreen <jessgreen2012@gmail.com>
2020-07-06 16:02:53 -0600

    Drill 191

---
+++ Drill 191/Drill 191/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_191
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number myNum = new Number() { Amount = .25M };
+            Console.WriteLine(myNum.Amount);
+            Console.ReadLine();
+        }
+        struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
