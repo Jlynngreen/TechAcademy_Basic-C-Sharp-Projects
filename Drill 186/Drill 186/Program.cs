9876150afc9eae6ca1fa4b2a418d823247b91570
Jlynngreen <jessgreen2012@gmail.com>
2020-07-06 14:17:38 -0600

    Drill 186

---
+++ Drill 186/Drill 186/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_186
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("user input the current day of the week?");
+
+            try
+            {
+                string userInput = Console.ReadLine();
+                DaysOfTheWeek Day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), (userInput));
+                Console.WriteLine("you picked the day: " + Day);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("please input actual day of the week");
+            }   
+
+
+            
+            Console.ReadLine();
+        }
+
+    }
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
+    }
+}
