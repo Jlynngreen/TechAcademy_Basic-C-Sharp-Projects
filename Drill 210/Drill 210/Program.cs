7796a886e294e46de46af1ab07d0590c6f53ee00
Jlynngreen <jessgreen2012@gmail.com>
2020-07-10 11:09:08 -0600

    Drill 210

---
+++ Drill 210/Drill 210/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Drill_210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User put in a number?");
+            float text = float.Parse(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter(@"C:\users\black\log1.txt", true))
+            {
+                file.WriteLine(text);
+            }
+
+               
+            Console.WriteLine(File.ReadAllText(@"C:\users\black\log1.txt"));
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
