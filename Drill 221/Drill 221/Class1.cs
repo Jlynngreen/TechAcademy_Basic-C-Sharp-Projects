1a0889bfaa88c602e1a3ebb74bc3f6e21e329ef0
Jlynngreen <jessgreen2012@gmail.com>
2020-07-10 17:09:24 -0600

    Drill 221

---
+++ Drill 221/Drill 221/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_221
+{
+    class Class1
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        
+        public Class1 ( int first, int second)
+        {
+            num1 = first;
+            num2 = second;
+        }
+        public Class1(int first): this (first, 4)
+        {
+
+        }
+        public int Substraction()
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ Drill 221/Drill 221/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "jess";
+            var age = "26";
+
+            var addition = new Class1(10);
+            Console.WriteLine(addition.Substraction());
+            Console.ReadLine();
+        }
+    }
+}
