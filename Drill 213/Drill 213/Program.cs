488248510cd72b449c2d94768aedb613f0337185
Jlynngreen <jessgreen2012@gmail.com>
2020-07-10 13:14:38 -0600

    Drill 213

---
+++ Drill 213/Drill 213/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("user input a number");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            DateTime date1 = DateTime.Now;
+            Console.WriteLine("the time in " + hour  + " hours will be " + date1.AddHours(hour));
+           
+            Console.ReadLine();
+        }
+    }
+}
