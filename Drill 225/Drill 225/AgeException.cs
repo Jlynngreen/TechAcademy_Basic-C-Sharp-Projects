0b6eabdf21a146e5e3ede4a33eda59d053ffd058
Jlynngreen <jessgreen2012@gmail.com>
2020-07-13 13:10:27 -0600

    Drill 225

---
+++ Drill 225/Drill 225/AgeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_225
+{
+    public  class AgeException : Exception
+    {
+        public AgeException()
+            : base(String.Format("please enter valid number")) { }
+
+    }
+}
---
+++ Drill 225/Drill 225/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_225
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter your age.");
+                int age = Convert.ToInt32(Console.ReadLine());
+                   
+                if (age < 0)
+                {
+                    throw new AgeException();
+                }
+                if (age == 0)
+                {
+                    throw new AgeException();
+                }
+
+                else
+                {
+                    Console.WriteLine("Year you were born in: " + DateTime.Now.AddYears(-age).Year);
+                    Console.ReadLine();
+                }   
+            }
+            catch (AgeException e)
+            {
+                Console.WriteLine("AgeIsNegativeException: {0}", e.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid age.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
